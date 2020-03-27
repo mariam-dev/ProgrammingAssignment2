b0b98d55dcb8352ad4d4f6322acf30d9782c7723
mt <mt@127.0.0.1localhost>
2020-03-26 20:30:37 -0600

    Code for assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,39 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
-
 makeCacheMatrix <- function(x = matrix()) {
-
+  
+  # 1. set the value of the matrix
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  
+  # 2. get the value of the matrix
+  
+  get <- function() x
+  
+  # 3. set the value of the matrix
+  
+  setmatrix <- function(matrix) m <<- matrix
+  
+  # 4. get the value of the matrix
+  
+  getmatrix <- function() m
+  list(set = set, get = get, setmatrix = setmatrix, getmatrix = getmatrix)
 }
 
 
-## Write a short comment describing this function
-
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  m <- x$getmatrix()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  inversem <- x$get()
+  m <- solve(inversem, ...)
+  x$setmatrix(m)
+  m
 }
